614c744cbf1f6f73bfd9873395fd82736aa7089a
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-05-30 13:47:51 +0000

    1.0

--- index.js
+++ index.js
@@ -5,22 +5,41 @@ const JSON = require("json")
 
 const app = express()
 
-app.get("/", (req, res) => {
+app.get("/kota", (req, res) => {
 
-  axios.get("https://www.jadwalsholat.org/").then(response => {
-    const data = []
+  axios.get("https://jadwalsholat.org/adzan/ajax.row.php?id=").then(response => {
+    const kota = req.query.search
     const $ = cheerio.load(response.data)
-    const table = $("div.widget-area")
-    const waktu = table.find("iframe").attr("data-src")
-    axios.get(waktu).then(response2 => {
-      const $ = cheerio.load(response2.data)
-    res.
-    res.json(response2.data)
+    const pilihKota = $("option").filter(function() {
+      return $(this).text() == kota
     })
-    
-    
+    const value = pilihKota.attr("value")
+    if (pilihKota.html()) {
+    axios.get("https://jadwalsholat.org/adzan/ajax/ajax.daily1.php?id=" + value).then(response => {
+      const $ = cheerio.load(response.data)
+      const waktuShubuh = $("tr.table_light").eq(0).find("td").eq(1)
+      const waktuDzuhur = $("tr.table_dark").eq(0).find("td").eq(1)
+      const waktuAshar = $("tr.table_light").eq(1).find("td").eq(1)
+      const waktuMaghrib = $("tr.table_dark").eq(1).find("td").eq(1)
+      const waktuIsya = $("tr.table_light").eq(2).find("td").eq(1)
+      res.json({
+      "kota" : pilihKota.html(),
+      "id" : value,
+      "waktuSholat" : {
+        "shubuh" : waktuShubuh.text(),
+        "dzuhur" : waktuDzuhur.text(),
+        "ashar" : waktuAshar.text(),
+        "mahgrib" : waktuMaghrib.text(),
+        "isya" : waktuIsya.text(),
+      }     
+      })
+    })
+  } else {
+      res.json({
+        "Error" : "Kota tidak ditemukan"
+      })
+  }
   })
-  
 })
 
 app.listen(8000, () => {
